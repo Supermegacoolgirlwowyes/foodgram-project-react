7933644a8e9bae18d40ef95ac1d60ecc03a458f2
supermegacoolgirlwowyes <supermegacoolgirlwowyes@gmail.com>
2022-01-25 03:37:34 +0300

    wokflow: image name mistake fixed

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -124,7 +124,7 @@ DJOSER = {
     'LOGIN_FIELD': 'email',
     'HIDE_USERS': False,
     'PERMISSIONS': {
-        'user': ['djoser.permissions.CurrentUserOrAdmin'],
+        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
         'user_list': ['rest_framework.permissions.AllowAny'],
     },
     'SERIALIZERS': {
