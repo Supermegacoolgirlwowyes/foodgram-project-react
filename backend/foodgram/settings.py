ae0c0dfe2a92d3b7bf4c5d9244195274ad477b5e
supermegacoolgirlwowyes <supermegacoolgirlwowyes@gmail.com>
2022-01-25 03:28:59 +0300

    settings: djoser user permissions v3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -124,7 +124,7 @@ DJOSER = {
     'LOGIN_FIELD': 'email',
     'HIDE_USERS': False,
     'PERMISSIONS': {
-        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
+        'user': ['djoser.permissions.CurrentUserOrAdmin'],
         'user_list': ['rest_framework.permissions.AllowAny'],
     },
     'SERIALIZERS': {
